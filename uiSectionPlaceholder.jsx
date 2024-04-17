abd10e1b36b84329c88a318a055387f19a098e4b
Luke Ferris <luke.ferris@gmail.com>
2024-04-17 11:50:11 +0200

    Updated ui Section placeholder

--- uiSectionPlaceholder.jsx
+++ uiSectionPlaceholder.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { Context } from './Context_Provider.jsx'; // we always import context, regardless of whether we use it or not
 
-const UISectionPlaceholder = ({ text }) => {
+// Default export of Companies_Component
+export default function [CELL_ID]_Component() {
   return (
     <div className="my-8 max-w-7xl px-2 sm:px-6 lg:px-8 py-6 border-2 border-dashed border-gray-400 rounded-lg">
-      <p className="text-center text-gray-600 text-lg italic">{text}</p>
+      <p className="text-center text-gray-600 text-lg italic">Placeholder text describing section goes here</p>
     </div>
   );
 };
-
-export default UISectionPlaceholder;
