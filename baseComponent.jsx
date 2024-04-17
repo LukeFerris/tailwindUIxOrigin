463fcc5cbfa6c1a1f0e1f141a3c6a6bda7782203
Luke Ferris <luke.ferris@gmail.com>
2024-04-17 10:27:28 +0200

    Added base component

---
+++ baseComponent.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { Context } from './Context_Provider.jsx'; // we always import context, regardless of whether we use it or not
+
+// Default export of Companies_Component
+export default function [CELL_ID]_Component() {
+  // Using Context to maintain a placeholder state
+  const context = useContext(Context);
+
+  // IMPORTANT: This component serves as a placeholder and does not interact with the context state or functionality.
+  return (
+    <div className="w-full align-top text-left space-y-8">
+      Content goes here
+    </div>
+  );
+}
